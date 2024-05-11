e7f1c243a082ca7d8616893b9c0809c474d3770a
EduardoTerres <eduardo.terrescaballero@outlook.com>
2024-05-11 18:48:40 +0200

    Auxiliary functions

--- plot_costs.m
+++ plot_costs.m
@@ -1,7 +1,26 @@
-function plot_costes(savecost, L2_distance, img_path_name)
-    f = figure('visible','off');
+% FUNCTION NAME:
+%   plot_costs
+%
+% AUTHORS:
+%       Eduardo Terrés and Julia Novo
+%
+% DESCRIPTION:
+%   Generate cost function and L2 error plot.
+%
+% INPUT:
+%   in1 - (vector) savecost: cost function in every iteration.
+%   in2 - (vector) L2_distance: L2 error in every iteration.
+%   in3 - (String) ticks_format: 'padded' or 'tickaligned'.
+%   in4 - (String) img_path_name: Full path of image to be saved, including
+%   name without extension.
+%
+% OUTPUT:
+%   None. The plot is generated and saved.
+function plot_costs(savecost, L2_distance, ticks_format, img_path_name)
+    f = figure('visible','on');
     f.Position = [0 0 900 600];
   
+    % Cost function
     semilogy(...
         1:1:size(savecost, 2), ...
         savecost(1:1:size(savecost, 2)), ...
@@ -11,25 +30,22 @@ function plot_costes(savecost, L2_distance, img_path_name)
     ytickformat('%.0e');
     ylabel('Cost function', 'FontSize', 12, 'Interpreter', 'latex');   
     
-    if L2_distance ~= 0
-        yyaxis right;
-        semilogy(...
-            1:1:size(L2_distance, 2), ...
-            L2_distance(1:1:size(L2_distance, 2)), ...
-            'Color', 'black', ...
-            'LineWidth', 2 ...
-            );
+    % L2 error
+    yyaxis right;
+    semilogy(...
+        1:1:size(L2_distance, 2), ...
+        L2_distance(1:1:size(L2_distance, 2)), ...
+        'Color', 'black', ...
+        'LineWidth', 2 ...
+        );
 
-        yticklabels(yticks);
-        ytickformat('%.0e');
-        ylabel('L2 error', 'FontSize', 12, 'Interpreter', 'latex');
-        legend('Cost function', 'L2 error', 'Interpreter', 'latex');
-    end
-    
+    yticklabels(yticks);
+    ytickformat('%.0e');
+    ylabel('L2 error', 'FontSize', 12, 'Interpreter', 'latex');    
     xlabel('Iteration number', 'FontSize', 12, 'Interpreter', 'latex');
     set(gca,'YColor','k');
-
-    ylim('padded');
+    ylim(ticks_format); % Padded or tickaligned
+    legend('Cost function', 'L2 error', 'Interpreter', 'latex');
 
     set(gca, 'TickLabelInterpreter', 'latex');
     set(gca, 'FontSize', 26);
--- plot_result.m
+++ plot_result.m
@@ -1,12 +1,41 @@
+% SCRIPT:
+%       fem_nn_1.m
+%
+% FUNCTION NAME:
+%   plot_results
+%
+% AUTHORS:
+%       Eduardo Terrés and Julia Novo
+%
+% DESCRIPTION:
+%   Generate theoretical and NN solution plot.
+%
+% INPUT:
+%   in1 - (function) F: neural network
+%   in2 - (vector) b2: biases
+%   in3 - (vector) W2: weights
+%   in4 - (vector) W3: weights
+%   in5 - (double) epsilon
+%   in6 - (integer) N: where N+1 is the number of grid points.
+%   in7 - (boolean) lines: If true plots vertical lines; if false doesnt.
+%   in8 - (String) img_path_name: Full path of image to be saved, including
+%   name without extension.
+%
+% OUTPUT:
+%   None. The plot is generated and saved.
 function plot_result(F, b2, W2, W3, epsilon, N, lines, img_path_name)
     f = figure('Units', 'inches', 'Position', [0, 0, 10, 7], ...
-        'visible','off');
+        'visible','on');
 
     plotting_grid = linspace(0, 1, 10000);
-    plot(plotting_grid, F(plotting_grid, b2, W2, W3), 'blue', 'linewidth', 2) % RRNN
-    y = @(x) (-exp(-1/epsilon)+exp((x-1)./epsilon))/(exp(-1/epsilon)-1) + x;
+    
+    % NN
+    plot(plotting_grid, F(plotting_grid, b2, W2, W3), 'blue', 'linewidth', 2)
     hold on
-    plot(plotting_grid, y(plotting_grid), 'red', 'linewidth', 2) % Teorica
+ 
+    % Theoretical
+    y = @(x) (-exp(-1/epsilon)+exp((x-1)./epsilon))/(exp(-1/epsilon)-1) + x;
+    plot(plotting_grid, y(plotting_grid), 'red', 'linewidth', 2)
 
     ylim('padded');
     set(gca, 'TickLabelInterpreter', 'latex');
@@ -15,8 +44,8 @@ function plot_result(F, b2, W2, W3, epsilon, N, lines, img_path_name)
     y_min = yLimits(1);
     y_max = yLimits(2);
 
-    lines_grid = linspace(0, 1, N+1);
     % Plot vertical lines
+    lines_grid = linspace(0, 1, N+1);
     if lines == true
         ylim('tight');
         for i = 1:length(lines_grid)
